adc718ab4994b9bc03cd1902662df984df7269ab
84605086 <84605086@qq.com>
2020-06-30 15:55:49 +0800

    fix

--- uni-app/config/app.js
+++ uni-app/config/app.js
@@ -3,14 +3,14 @@ module.exports = {
 	// 请求域名 格式： https://您的域名
 	// #ifdef MP
 	HTTP_REQUEST_URL:'https://www.ipinhai.com',
-	HTTP_REQUEST_URL_NEW:'http://youpin.xiaosongzhixue.com',
+	HTTP_REQUEST_URL_NEW:'https://youpin.xiaosongzhixue.com',
 	// #endif
 	
 	// #ifdef H5
 		//H5接口是浏览器地址
 		//HTTP_REQUEST_URL: window.location.protocol+"//"+window.location.host,
 		HTTP_REQUEST_URL:'https://www.ipinhai.com',
-		HTTP_REQUEST_URL_NEW:'http://youpin.xiaosongzhixue.com',
+		HTTP_REQUEST_URL_NEW:'https://youpin.xiaosongzhixue.com',
 	// #endif
 	
 	
